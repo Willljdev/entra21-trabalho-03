2c62317c72476ada11c157f9e388be71d17747a7
GustavoPrim <gugahprm@gmail.com>
2022-07-24 12:25:59 -0300

    Finalizado o PosicaoService

--- entra21-trabalho-03/Services/PosicaoService.cs
+++ entra21-trabalho-03/Services/PosicaoService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,17 +37,66 @@ namespace entra21_trabalho_03.Services
 
         public void Editar(Posicao posicao)
         {
-            throw new NotImplementedException();
+            var conexao = new Conexao().Conectar();
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = "UPDATE posicoes SET nome = @NOME WHERE id = @ID";
+            comando.Parameters.AddWithValue("@NOME", posicao.Nome);
+            comando.Parameters.AddWithValue("@ID", posicao.Id);
+
+            comando.ExecuteNonQuery();
+            conexao.Close();
         }
 
         public Posicao ObterPorId(int id)
         {
-            throw new NotImplementedException();
+            var conexao = new Conexao().Conectar();
+            var comando = conexao.CreateCommand();
+            
+            comando.CommandText = "SELECT id, nome FROM posicoes WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+
+            var tabelaMemoria = new DataTable();
+            tabelaMemoria.Load(comando.ExecuteReader());
+
+            if (tabelaMemoria.Rows.Count == 0)
+                return null;
+
+            var registro = tabelaMemoria.Rows[0];
+            var posicao = new Posicao();
+
+            posicao.Id = Convert.ToInt32(registro["id"]);
+            posicao.Nome = registro["nome"].ToString();
+
+            conexao.Close();
+            return posicao;
         }
 
         public List<Posicao> ObterTodos()
         {
-            throw new NotImplementedException();
+            var conexao = new Conexao().Conectar();
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = "SELECT id, nome FROM posicoes";
+
+            var tabelaMemoria = new DataTable();
+            tabelaMemoria.Load(comando.ExecuteReader());
+
+            var posicoes = new List<Posicao>();
+
+            for (var i = 0; i < tabelaMemoria.Rows.Count; i++)
+            {
+                var linha = tabelaMemoria.Rows[i];
+                
+                var posicao = new Posicao();
+                posicao.Id = Convert.ToInt32(linha["id"].ToString());
+                posicao.Nome = linha["nome"].ToString();
+
+                posicoes.Add(posicao);
+            }
+            conexao.Close();
+
+            return posicoes;
         }
     }
 }
