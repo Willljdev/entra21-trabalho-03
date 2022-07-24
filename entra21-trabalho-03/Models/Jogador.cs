b5987b4d5ab274434a3c1bb6e6c00f4f311612b2
GustavoPrim <gugahprm@gmail.com>
2022-07-24 11:32:19 -0300

    Criado as pastas do trabalho e adicionado o Models de Posição e Jogador

---
+++ entra21-trabalho-03/Models/Jogador.cs
@@ -0,0 +1,11 @@
+﻿namespace entra21_trabalho_03.Models
+{
+    public class Jogador
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Posicao Posicao { get; set; }
+    }
+}
---
+++ entra21-trabalho-03/Models/Posicao.cs
@@ -0,0 +1,8 @@
+﻿namespace entra21_trabalho_03.Models
+{
+    public class Posicao
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
