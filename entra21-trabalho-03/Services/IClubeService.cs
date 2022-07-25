064fe40a622e1837333b43ac387a036083e71cfb
willljdev <willljdev@gmail.com>
2022-07-25 20:48:39 -0300

    Criação IClubeService

---
+++ entra21-trabalho-03/Services/IClubeService.cs
@@ -0,0 +1,18 @@
+﻿using entra21_trabalho_03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entra21_trabalho_03.Services
+{
+    internal interface IClubeService
+    {
+        List<Clube> ObterTodos();
+        Clube ObterPorId(int id);
+        void Cadastrar(Clube clube);
+        void Editar (Clube clube);
+        void Apagar(int id);
+    }
+}
