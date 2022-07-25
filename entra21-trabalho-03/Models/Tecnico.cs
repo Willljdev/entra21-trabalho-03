c2de00e51813dff3b95c5713c13ac0c6b61515b3
willljdev <willljdev@gmail.com>
2022-07-25 19:38:49 -0300

    Criação do model Tecnico.

---
+++ entra21-trabalho-03/Models/Tecnico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entra21_trabalho_03.Models
+{
+    internal class Tecnico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string CidadeNatal { get; set; }
+        public Clube Clube { get; set; }
+    }
+}
