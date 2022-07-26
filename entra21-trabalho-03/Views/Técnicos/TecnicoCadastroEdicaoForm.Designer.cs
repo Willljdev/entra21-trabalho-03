19c4c9dc82d583675c5a7a59c847389cdeeab27f
willljdev <willljdev@gmail.com>
2022-07-25 21:45:00 -0300

    Criação do Form TecnicoCadastroEdicaoForm.

---
+++ entra21-trabalho-03/Views/Técnicos/TecnicoCadastroEdicaoForm.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace entra21_trabalho_03.Views.Técnicos
+{
+    partial class TecnicoCadastroEdicaoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelCpf = new System.Windows.Forms.Label();
+            this.maskedTextBoxCpf = new System.Windows.Forms.MaskedTextBox();
+            this.labelDataNascimento = new System.Windows.Forms.Label();
+            this.dateTimePickerDataNascimento = new System.Windows.Forms.DateTimePicker();
+            this.labelCidadenatal = new System.Windows.Forms.Label();
+            this.textBoxCidadeNatal = new System.Windows.Forms.TextBox();
+            this.comboBoxClubeAtual = new System.Windows.Forms.ComboBox();
+            this.labelClubeAtual = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelNome1 = new System.Windows.Forms.Label();
+            this.labelCpf1 = new System.Windows.Forms.Label();
+            this.labelDataNascimento1 = new System.Windows.Forms.Label();
+            this.labelCidadeNatal1 = new System.Windows.Forms.Label();
+            this.textBoxNome1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBoxCpf1 = new System.Windows.Forms.MaskedTextBox();
+            this.dateTimePickerDataNascimento1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelClubeAtual1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(12, 32);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(305, 27);
+            this.textBoxNome.TabIndex = 2;
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(12, 9);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(50, 20);
+            this.labelNome.TabIndex = 3;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelCpf
+            // 
+            this.labelCpf.AutoSize = true;
+            this.labelCpf.Location = new System.Drawing.Point(12, 62);
+            this.labelCpf.Name = "labelCpf";
+            this.labelCpf.Size = new System.Drawing.Size(33, 20);
+            this.labelCpf.TabIndex = 4;
+            this.labelCpf.Text = "CPF";
+            // 
+            // maskedTextBoxCpf
+            // 
+            this.maskedTextBoxCpf.Location = new System.Drawing.Point(12, 85);
+            this.maskedTextBoxCpf.Name = "maskedTextBoxCpf";
+            this.maskedTextBoxCpf.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBoxCpf.TabIndex = 5;
+            // 
+            // labelDataNascimento
+            // 
+            this.labelDataNascimento.AutoSize = true;
+            this.labelDataNascimento.Location = new System.Drawing.Point(12, 115);
+            this.labelDataNascimento.Name = "labelDataNascimento";
+            this.labelDataNascimento.Size = new System.Drawing.Size(145, 20);
+            this.labelDataNascimento.TabIndex = 6;
+            this.labelDataNascimento.Text = "Data de Nascimento";
+            // 
+            // dateTimePickerDataNascimento
+            // 
+            this.dateTimePickerDataNascimento.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePickerDataNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerDataNascimento.Location = new System.Drawing.Point(12, 138);
+            this.dateTimePickerDataNascimento.Name = "dateTimePickerDataNascimento";
+            this.dateTimePickerDataNascimento.Size = new System.Drawing.Size(111, 27);
+            this.dateTimePickerDataNascimento.TabIndex = 7;
+            // 
+            // labelCidadenatal
+            // 
+            this.labelCidadenatal.AutoSize = true;
+            this.labelCidadenatal.Location = new System.Drawing.Point(12, 168);
+            this.labelCidadenatal.Name = "labelCidadenatal";
+            this.labelCidadenatal.Size = new System.Drawing.Size(96, 20);
+            this.labelCidadenatal.TabIndex = 8;
+            this.labelCidadenatal.Text = "Cidade Natal";
+            // 
+            // textBoxCidadeNatal
+            // 
+            this.textBoxCidadeNatal.Location = new System.Drawing.Point(12, 191);
+            this.textBoxCidadeNatal.Name = "textBoxCidadeNatal";
+            this.textBoxCidadeNatal.Size = new System.Drawing.Size(305, 27);
+            this.textBoxCidadeNatal.TabIndex = 9;
+            // 
+            // comboBoxClubeAtual
+            // 
+            this.comboBoxClubeAtual.FormattingEnabled = true;
+            this.comboBoxClubeAtual.Location = new System.Drawing.Point(12, 244);
+            this.comboBoxClubeAtual.Name = "comboBoxClubeAtual";
+            this.comboBoxClubeAtual.Size = new System.Drawing.Size(151, 28);
+            this.comboBoxClubeAtual.TabIndex = 10;
+            // 
+            // labelClubeAtual
+            // 
+            this.labelClubeAtual.AutoSize = true;
+            this.labelClubeAtual.Location = new System.Drawing.Point(12, 221);
+            this.labelClubeAtual.Name = "labelClubeAtual";
+            this.labelClubeAtual.Size = new System.Drawing.Size(84, 20);
+            this.labelClubeAtual.TabIndex = 11;
+            this.labelClubeAtual.Text = "Clube atual";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            // 
+            // labelNome1
+            // 
+            this.labelNome1.AutoSize = true;
+            this.labelNome1.Location = new System.Drawing.Point(12, 9);
+            this.labelNome1.Name = "labelNome1";
+            this.labelNome1.Size = new System.Drawing.Size(50, 20);
+            this.labelNome1.TabIndex = 3;
+            this.labelNome1.Text = "Nome";
+            // 
+            // labelCpf1
+            // 
+            this.labelCpf1.AutoSize = true;
+            this.labelCpf1.Location = new System.Drawing.Point(12, 62);
+            this.labelCpf1.Name = "labelCpf1";
+            this.labelCpf1.Size = new System.Drawing.Size(33, 20);
+            this.labelCpf1.TabIndex = 4;
+            this.labelCpf1.Text = "CPF";
+            // 
+            // labelDataNascimento1
+            // 
+            this.labelDataNascimento1.AutoSize = true;
+            this.labelDataNascimento1.Location = new System.Drawing.Point(12, 115);
+            this.labelDataNascimento1.Name = "labelDataNascimento1";
+            this.labelDataNascimento1.Size = new System.Drawing.Size(145, 20);
+            this.labelDataNascimento1.TabIndex = 5;
+            this.labelDataNascimento1.Text = "Data de Nascimento";
+            // 
+            // labelCidadeNatal1
+            // 
+            this.labelCidadeNatal1.AutoSize = true;
+            this.labelCidadeNatal1.Location = new System.Drawing.Point(12, 168);
+            this.labelCidadeNatal1.Name = "labelCidadeNatal1";
+            this.labelCidadeNatal1.Size = new System.Drawing.Size(96, 20);
+            this.labelCidadeNatal1.TabIndex = 6;
+            this.labelCidadeNatal1.Text = "Cidade Natal";
+            // 
+            // textBoxNome1
+            // 
+            this.textBoxNome1.Location = new System.Drawing.Point(12, 32);
+            this.textBoxNome1.Name = "textBoxNome1";
+            this.textBoxNome1.Size = new System.Drawing.Size(395, 27);
+            this.textBoxNome1.TabIndex = 7;
+            // 
+            // maskedTextBoxCpf1
+            // 
+            this.maskedTextBoxCpf1.Location = new System.Drawing.Point(12, 85);
+            this.maskedTextBoxCpf1.Name = "maskedTextBoxCpf1";
+            this.maskedTextBoxCpf1.Size = new System.Drawing.Size(190, 27);
+            this.maskedTextBoxCpf1.TabIndex = 8;
+            // 
+            // dateTimePickerDataNascimento1
+            // 
+            this.dateTimePickerDataNascimento1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePickerDataNascimento1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerDataNascimento1.Location = new System.Drawing.Point(12, 138);
+            this.dateTimePickerDataNascimento1.Name = "dateTimePickerDataNascimento1";
+            this.dateTimePickerDataNascimento1.Size = new System.Drawing.Size(107, 27);
+            this.dateTimePickerDataNascimento1.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 27);
+            this.textBox1.TabIndex = 10;
+            // 
+            // labelClubeAtual1
+            // 
+            this.labelClubeAtual1.AutoSize = true;
+            this.labelClubeAtual1.Location = new System.Drawing.Point(12, 221);
+            this.labelClubeAtual1.Name = "labelClubeAtual1";
+            this.labelClubeAtual1.Size = new System.Drawing.Size(84, 20);
+            this.labelClubeAtual1.TabIndex = 11;
+            this.labelClubeAtual1.Text = "Clube atual";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 244);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 28);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 53);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Cancelar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(313, 354);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 53);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Salvar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(151, 354);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 53);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Menu";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // TecnicoCadastroEdicaoForm
+            // 
+            this.ClientSize = new System.Drawing.Size(436, 424);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelClubeAtual1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePickerDataNascimento1);
+            this.Controls.Add(this.maskedTextBoxCpf1);
+            this.Controls.Add(this.textBoxNome1);
+            this.Controls.Add(this.labelCidadeNatal1);
+            this.Controls.Add(this.labelDataNascimento1);
+            this.Controls.Add(this.labelCpf1);
+            this.Controls.Add(this.labelNome1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "TecnicoCadastroEdicaoForm";
+            this.Text = "Cadastro de Técnicos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBoxNome;
+        private Label labelNome;
+        private Label labelCpf;
+        private MaskedTextBox maskedTextBoxCpf;
+        private Label labelDataNascimento;
+        private DateTimePicker dateTimePickerDataNascimento;
+        private Label labelCidadenatal;
+        private TextBox textBoxCidadeNatal;
+        private ComboBox comboBoxClubeAtual;
+        private Label labelClubeAtual;
+        private Button button1;
+        private Button button2;
+        private Label labelNome1;
+        private Label labelCpf1;
+        private Label labelDataNascimento1;
+        private Label labelCidadeNatal1;
+        private TextBox textBoxNome1;
+        private MaskedTextBox maskedTextBoxCpf1;
+        private DateTimePicker dateTimePickerDataNascimento1;
+        private TextBox textBox1;
+        private Label labelClubeAtual1;
+        private ComboBox comboBox1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
