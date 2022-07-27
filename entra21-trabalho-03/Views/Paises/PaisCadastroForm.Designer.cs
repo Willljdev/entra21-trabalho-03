2f1ef96a729ad6e346d708ea3914ec45bdddc34d
Lucas Perez Alves <perezcfnpbnu@gmail.com>
2022-07-26 21:10:31 -0300

    Feito a tela de cadastroPaises e terminado o service de competição

---
+++ entra21-trabalho-03/Views/Paises/PaisCadastroForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace entra21_trabalho_03.Views.Paises
+{
+    partial class PaisCadastroForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelPais = new System.Windows.Forms.Label();
+            this.labelContinente = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxContinente = new System.Windows.Forms.TextBox();
+            this.comboBoxEstado = new System.Windows.Forms.ComboBox();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonMenu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(12, 27);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(121, 23);
+            this.textBoxNome.TabIndex = 0;
+            // 
+            // labelPais
+            // 
+            this.labelPais.AutoSize = true;
+            this.labelPais.Location = new System.Drawing.Point(12, 9);
+            this.labelPais.Name = "labelPais";
+            this.labelPais.Size = new System.Drawing.Size(28, 15);
+            this.labelPais.TabIndex = 1;
+            this.labelPais.Text = "País";
+            // 
+            // labelContinente
+            // 
+            this.labelContinente.AutoSize = true;
+            this.labelContinente.Location = new System.Drawing.Point(12, 75);
+            this.labelContinente.Name = "labelContinente";
+            this.labelContinente.Size = new System.Drawing.Size(66, 15);
+            this.labelContinente.TabIndex = 2;
+            this.labelContinente.Text = "Continente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Estado";
+            // 
+            // textBoxContinente
+            // 
+            this.textBoxContinente.Location = new System.Drawing.Point(12, 93);
+            this.textBoxContinente.Name = "textBoxContinente";
+            this.textBoxContinente.Size = new System.Drawing.Size(121, 23);
+            this.textBoxContinente.TabIndex = 4;
+            // 
+            // comboBoxEstado
+            // 
+            this.comboBoxEstado.FormattingEnabled = true;
+            this.comboBoxEstado.Location = new System.Drawing.Point(12, 149);
+            this.comboBoxEstado.Name = "comboBoxEstado";
+            this.comboBoxEstado.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxEstado.TabIndex = 5;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(165, 199);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalvar.TabIndex = 6;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(3, 199);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 7;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
+            // 
+            // buttonMenu
+            // 
+            this.buttonMenu.Location = new System.Drawing.Point(84, 199);
+            this.buttonMenu.Name = "buttonMenu";
+            this.buttonMenu.Size = new System.Drawing.Size(75, 23);
+            this.buttonMenu.TabIndex = 8;
+            this.buttonMenu.Text = "Menu";
+            this.buttonMenu.UseVisualStyleBackColor = true;
+            this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
+            // 
+            // PaisCadastroForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 414);
+            this.Controls.Add(this.buttonMenu);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.comboBoxEstado);
+            this.Controls.Add(this.textBoxContinente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelContinente);
+            this.Controls.Add(this.labelPais);
+            this.Controls.Add(this.textBoxNome);
+            this.Name = "PaisCadastroForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PaisCadastroForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBoxNome;
+        private Label labelPais;
+        private Label labelContinente;
+        private Label label2;
+        private TextBox textBoxContinente;
+        private ComboBox comboBoxEstado;
+        private Button buttonSalvar;
+        private Button buttonCancelar;
+        private Button buttonMenu;
+    }
+}
---
+++ entra21-trabalho-03/Views/Paises/PaisCadastroForm.cs
@@ -0,0 +1,71 @@
+﻿using entra21_trabalho_03.PaisesCompeticoes.Models;
+using entra21_trabalho_03.PaisesCompeticoes.Services;
+
+namespace entra21_trabalho_03.Views.Paises
+{
+    public partial class PaisCadastroForm : Form
+    {
+        private int id = -1;
+        public PaisCadastroForm()
+        {
+            InitializeComponent();
+        }
+
+        public PaisCadastroForm(Pais paises) : this()
+        {
+            id = paises.Id;
+            textBoxNome.Text = paises.Nome;
+            textBoxContinente.Text = paises.Continente;
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            var nome = textBoxNome.Text.Trim();
+            var continente = textBoxContinente.Text.Trim();
+
+            if(nome.Length < 3 || nome.Length > 50)
+            {
+                MessageBox.Show("Digita um nome descente ai fio");
+                textBoxNome.ResetText();
+                textBoxNome.Focus();
+                return;
+            }
+
+            if(continente.Length < 3 || continente.Length > 50)
+            {
+                MessageBox.Show("Digita um continente maior ai");
+                textBoxContinente.ResetText();
+                textBoxContinente.Focus();
+                return;
+            }
+
+            var pais = new Pais();
+            pais.Nome = nome;
+            pais.Continente = continente;
+
+            var paisService = new PaisService();
+            if(id == -1)
+            {
+                paisService.Cadastrar(pais);
+                MessageBox.Show("Cadastrados com sucessos!!");
+                Close();
+                return;
+            }
+
+            pais.Id = id;
+            paisService.Editar(pais);
+            MessageBox.Show("Alterados!!");
+            Close();
+        }
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
