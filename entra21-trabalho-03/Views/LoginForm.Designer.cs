ef2b3a21c2414522682923948ac604812fb40d3f
Lucas Perez Alves <perezcfnpbnu@gmail.com>
2022-07-30 17:06:23 -0300

    realizado a tela de login

---
+++ entra21-trabalho-03/Views/LoginForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace entra21_trabalho_03.Views
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEntrar = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.linkLabelSuporte = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // buttonEntrar
+            // 
+            this.buttonEntrar.Location = new System.Drawing.Point(44, 273);
+            this.buttonEntrar.Name = "buttonEntrar";
+            this.buttonEntrar.Size = new System.Drawing.Size(152, 46);
+            this.buttonEntrar.TabIndex = 0;
+            this.buttonEntrar.Text = "Entrar";
+            this.buttonEntrar.UseVisualStyleBackColor = true;
+            this.buttonEntrar.Click += new System.EventHandler(this.buttonEntrar_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(12, 126);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(210, 23);
+            this.textBoxLogin.TabIndex = 1;
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(12, 206);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.Size = new System.Drawing.Size(210, 23);
+            this.textBoxSenha.TabIndex = 2;
+            this.textBoxSenha.Enter += new System.EventHandler(this.textBoxSenha_Enter);
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(12, 108);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(37, 15);
+            this.labelLogin.TabIndex = 3;
+            this.labelLogin.Text = "Login";
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.Location = new System.Drawing.Point(10, 188);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(39, 15);
+            this.labelSenha.TabIndex = 4;
+            this.labelSenha.Text = "Senha";
+            // 
+            // linkLabelSuporte
+            // 
+            this.linkLabelSuporte.AutoSize = true;
+            this.linkLabelSuporte.Location = new System.Drawing.Point(221, 380);
+            this.linkLabelSuporte.Name = "linkLabelSuporte";
+            this.linkLabelSuporte.Size = new System.Drawing.Size(48, 15);
+            this.linkLabelSuporte.TabIndex = 5;
+            this.linkLabelSuporte.TabStop = true;
+            this.linkLabelSuporte.Text = "Suporte";
+            this.linkLabelSuporte.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSuporte_LinkClicked);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 404);
+            this.Controls.Add(this.linkLabelSuporte);
+            this.Controls.Add(this.labelSenha);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.buttonEntrar);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonEntrar;
+        private TextBox textBoxLogin;
+        private TextBox textBoxSenha;
+        private Label labelLogin;
+        private Label labelSenha;
+        private LinkLabel linkLabelSuporte;
+    }
+}
---
+++ entra21-trabalho-03/Views/LoginForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entra21_trabalho_03.Views
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEntrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(textBoxLogin.Text.Equals("Grupo003") && textBoxSenha.Text.Equals("Calabresa"))
+                {
+                    var listarMenu = new MenuPrincipalForm();
+                    listarMenu.Show();
+
+                    this.Visible = false;
+                }
+                else
+                {
+                    MessageBox.Show("Login ou Senha são inválidos!!",
+                        "Avisado você foi!!",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    textBoxLogin.Focus();
+                    textBoxSenha.Text = "";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Aviso DENOVO!",
+                    ex.Message,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void linkLabelSuporte_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var enderecoSuporte = "https://www.linkedin.com/in/lucas-perez-86721a242/";
+            var psi = new System.Diagnostics.ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = enderecoSuporte;
+            System.Diagnostics.Process.Start(psi);
+        }
+
+        private void textBoxSenha_Enter(object sender, EventArgs e)
+        {
+            textBoxSenha.Text = "";
+            textBoxSenha.Focus();
+        }
+    }
+}
